63824dd8a4581d7e219659da02c371a298adf7b1
Wallauerr <wallauer@protonmail.com>
2024-02-21 22:23:58 -0300

    installed tanstack and created interface for pages and items

--- src/app.tsx
+++ src/app.tsx
@@ -12,8 +12,42 @@ import {
   TableRow,
 } from './components/ui/table'
 import { Pagination } from './components/pagination'
+import { useQuery } from '@tanstack/react-query'
+
+export interface TagResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
+
+export interface Tag {
+  title: string
+  slug: string
+  amountOfVideos: number
+  id: string
+}
 
 export function App() {
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ['get-tags'],
+    queryFn: async () => {
+      const response = await fetch(
+        'http://localhost:3333/tags?_page=1&_per_page=10',
+      )
+      const data = await response.json()
+
+      return data
+    },
+  })
+
+  if (isLoading) {
+    return null
+  }
+
   return (
     <div className="py-10 space-y-8">
       <div>
@@ -52,19 +86,19 @@ export function App() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {Array.from({ length: 10 }).map((_value, index) => {
+            {tagsResponse?.data.map((tag) => {
               return (
-                <TableRow key={index}>
+                <TableRow key={tag.id}>
                   <TableCell></TableCell>
                   <TableCell>
                     <div className="flex flex-col gap-0.5">
-                      <span className="font-medium">React</span>
-                      <span className="text-xs text-zinc-500">
-                        d4f095ed-b895-4e16-abb0-825b3528ef34
-                      </span>
+                      <span className="font-medium">{tag.title}</span>
+                      <span className="text-xs text-zinc-500">{tag.id}</span>
                     </div>
                   </TableCell>
-                  <TableCell className="text-zinc-300">13 video(s)</TableCell>
+                  <TableCell className="text-zinc-300">
+                    {tag.amountOfVideos} video(s)
+                  </TableCell>
                   <TableCell className="text-right">
                     <Button size="icon">
                       <MoreHorizontal className="size-4" />
@@ -76,7 +110,13 @@ export function App() {
           </TableBody>
         </Table>
 
-        <Pagination />
+        {tagsResponse && (
+          <Pagination
+            pages={tagsResponse.pages}
+            items={tagsResponse.items}
+            page={1}
+          />
+        )}
       </main>
     </div>
   )
--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -13,10 +13,16 @@ import {
   SelectTrigger,
 } from '../components/ui/select'
 
-export function Pagination() {
+interface PaginationProps {
+  pages: number
+  items: number
+  page: number
+}
+
+export function Pagination({ items, page, pages }: PaginationProps) {
   return (
     <div className="flex text-sm items-center justify-between text-zinc-500">
-      <span>Showing 10 of 228 items</span>
+      <span>Showing 10 of {items} items</span>
       <div className="flex items-center gap-8">
         <div className="flex items-center gap-2">
           <span>Rows per page</span>
@@ -31,7 +37,9 @@ export function Pagination() {
           </Select>
         </div>
 
-        <span>Page 1 of 10</span>
+        <span>
+          Page {page} of {pages}
+        </span>
 
         <div className="space-x-1.5">
           <Button size="icon">
